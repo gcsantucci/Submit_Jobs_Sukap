65d9402887aacc47604022a4000c22798ab3ca4e
Gabriel Santucci <gabriel.santucci@stonybrook.edu>
2016-12-15 09:30:27 +0900

    changed log

--- help_functions.py
+++ help_functions.py
@@ -4,11 +4,18 @@ import sys
 import os
 import getopt
 import shutil
+import time
+import logging
 
 from parameters import default_card
 
-def call_exit():
-    print('Exiting...')
+log = logging
+
+def call_exit(islog=0):
+    msg = 'Exiting...'
+    print(msg)
+    if islog:
+        log_msg(msg, error=True)
     sys.exit(0)
 
 def usage():
@@ -33,12 +40,12 @@ def get_card():
     card = None
     if len(opts) == 0:
         usage()
-        call_exit()
+        call_exit(0)
         
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             usage()
-            call_exit()
+            call_exit(0)
         elif opt in ('--card'):
             card = arg
         else:
@@ -49,41 +56,59 @@ def get_card():
     is_card(card)
     return card
 
-#Great function to implement try except catch logic!! Study and implement this! 
 def is_card(card):
-    if os.path.isfile(card):
-        print('\nUsing card: {0}'.format(card))
-    else:
-        print('Parameters card does not exist!')
-        call_exit()
+    if not os.path.isfile(card):
+        print('Parameters card does not exist:\n{0}'.format(card))
+        call_exit(0)
 
-def check_dirs(dirtype, jobpath, nfiles, start, nsubjobs):
+def start_log(logfile, card):
+    log.basicConfig(filename=logfile, format='%(message)s', level=logging.ERROR)
+    hf.log_msg('Location of log file:\n{0}'.format(logfile))
+    hf.log_msg('Using card:\n{0}'.format(card))
+    hf.log_msg(hf.get_time(mode='start'))
+
+def log_msg(msg, error=False):
+    print(msg)
+    if error:
+        log.error('Error!\n' + msg)
+    else:
+        log.info(msg)
+
+def check_dirs(dirtype, jobpath, nfiles, start, nsubjobs, logfile, card):
     if os.path.isdir(jobpath):
-        print('Dir {0} already exists.'.format(jobpath))
+        start_log(logfile, card)
+        msg = 'Dir {0} already exists.\n'.format(jobpath)
         if dirtype == 'new':
-            print('But user set job as new.')
-            call_exit()
+            msg += 'But user set job as new.'
+            log_msg(msg)
+            call_exit(1)
         elif dirtype == 'same':
-            print('Using the same directory.')
+            msg += 'Using the same directory.'
+            log_msg(msg)
         elif dirtype == 'over':
-            print('Removing old dir and creating new.')
+            msg += 'Removing old dir and creating new.'
+            log_msg(msg)
             shutil.rmtree(jobpath)
             make_dirs(jobpath, nfiles, start, nsubjobs)
         else:
-            print('Enter [new], [same] or [over] for dirtype in the parameters.card')
-            call_exit()
+            msg += 'Enter [new], [same] or [over] for dirtype in the parameters.card'
+            log_msg(msg)            
+            call_exit(1)
     else:
-        print('Dir {0} does not exist.'.format(jobpath))
+        msg = 'Dir {0} does not exist.\n'.format(jobpath)
         if dirtype == 'new':
-            print('Creating necessary directory structure')
+            start_log(logfile, card)
+            msg += 'Creating necessary directory structure'
+            log_msg(msg)
             make_dirs(jobpath, nfiles, start, nsubjobs)
         elif dirtype == 'same' or dirtype == 'over':
-            print('Output directory was not found:\n{0}'.format(jobpath))
-            print('Change parameters.card.')
-            call_exit()
+            msg += 'Output directory was not found:\n{0}'.format(jobpath)
+            msg += '\nChange parameters.card.'
+            print(msg)
+            call_exit(0)
         else:
-            print('Enter [new], [same] or [over] for dirtype in the parameters.card')
-            call_exit()
+            print(msg + 'Enter [new], [same] or [over] for dirtype in the parameters.card')
+            call_exit(0)
 
 def make_dirs(jobpath, nfiles, start, nsubjobs):
     os.mkdir(jobpath)    
@@ -93,11 +118,6 @@ def make_dirs(jobpath, nfiles, start, nsubjobs):
         for j in xrange(nsubjobs):
             isub = os.path.join(ijob, str(j))
             os.mkdir(isub)
-
-def is_dir(jobpath):
-    if not os.path.isdir(jobpath):
-        print('Output directory was not found:\n{0}'.format(jobpath))
-        call_exit()
 
 def number_jobs(maxjobs, nsubjobs, nfiles):
     if maxjobs == -1:
@@ -115,6 +135,35 @@ def check_njobs(cmd, log):
     with open(log, 'r') as njobslog:
         njobs = int(njobslog.read().splitlines()[0])
     return njobs
+
+def get_time(mode=None, sub=False):
+    time = time.asctime( time.localtime(time.time()) )
+    cur_time = '\nCurrent time: {0}\n'.format(time)
+    msg = cur_time.format(time)
+    if mode == 'start':
+        msg += 'Starting submit_jobs.py'
+    elif mode == 'file':
+        msg += 'Starting input file {0}:\n{1}'
+    elif mode == 'isub':
+        msg += 'Starting subjob {0} for input file:\n{1}'
+    elif mode == 'sleep':
+        msg += '{0} jobs already running. Sleeping for {1} minutes.'
+    elif mode == 'end':
+        msg += 'Finish sending all the jobs!'
+    else:
+        print('Unknown mode for get_time.')
+        call_exit(1)
+    return msg
+
+def get_email(isub=False):
+    email = 'echo "Sent infile {0} {1} to queue." | mailx -s "Job Sent" {2}'
+    if email:
+        email ='echo "Sent infile {0} {1} - subjob {2} - to queue." | mailx -s "Job Sent" {3}'
+    return email
+
+def send_email(email):
+    log.info(email)
+    os.system(email)
 
 def WriteSKBash(cmdstr, jobdir):
     name = jobdir + 'NQSjob_nglogL.csh'
--- submit_jobs.py
+++ submit_jobs.py
@@ -8,11 +8,6 @@ gabriel.santucci@stonybrook.edu
 Dec 2016
 '''
 
-import sys
-import os
-import time
-import logging
-
 import help_functions as hf
 from parameters import get_params
 
@@ -23,40 +18,32 @@ def send_jobs(card):
     inpath, ext, outpath, outdirname, outdirtype, jobname, email, queue, user = strparams
     maxjobs = hf.number_jobs(maxjobs, nsubjobs, nfiles)
 
-    # Create directory structure:
+    # Create directory structure and log file:
     jobpath = os.path.join(outpath, outdirname)
-    hf.check_dirs(outdirtype, jobpath, nfiles, startfile, nsubjobs)
-
-    # Write log file in output directory:
     logfile = os.path.join(jobpath, jobname + '.log')
-    logging.basicConfig(filename=logfile, format='%(message)s', level=logging.INFO)
-    print('\nLog file:\n{0}'.format(logfile))
+    hf.check_dirs(outdirtype, jobpath, nfiles, startfile, nsubjobs, logfile, card)
 
-    queue_log = os.path.join(jobpath, '{0}_njobs.log'.format(queue))
+    queue_log = os.path.join(jobpath, 'currentjobs_{0}.log'.format(queue))
     queue_cmd = ('qstat {0} | grep {1} | wc -l > ' + queue_log).format(queue, user)
-    email_cmd = 'echo "Sent infile {0} {1} to queue." | mailx -s "Job Sent" {2}'
-    email_sub ='echo "Sent infile {0} {1}  - subjob {2} - to queue." | mailx -s "Job Sent" {3}'
 
     infiles = hf.get_infiles(inpath, ext, startfile, nfiles)
     for i, infile in enumerate(infiles, start=startfile):
-        logging.info('Input file {0}: {1}'.format(i, infile))
+        hf.log_msg(hf.get_time(mode='file').format(i, infile))
         isub = 0
         while isub < int(nsubjobs):
-            print isub
-            logging.info('Subjob = {0}'.format(isub))
+            hf.log_msg(hf.get_time(mode='isub').format(isub, infile))
             currentjobs = hf.check_njobs(queue_cmd, queue_log)
             if currentjobs < maxjobs:
                 #hf.runfiTQun(ijob, jobname, path, isub, nevents)
                 if email and subrate > 0 and isub % subrate == subrate-1:
-                    os.system(email_sub.format(i, infile, isub, email))
+                    hf.send_email(hf.get_email(isub=True).format(i, infile, isub, email))
                 isub += 1
             else:
-                logging.info(time.asctime( time.localtime(time.time()) ))
-                logging.info('sleeping {0} minutes.'.format(sleeptime))
+                hf.log_msg(hf.get_time(mode='sleep').format(maxjobs, sleeptime))
                 time.sleep(60*sleeptime)
         if email and i % emailrate == emailrate-1:
-            os.system(email_cmd.format(i, infile, email))
-    logging.info('Sent all the jobs!')
+            hf.send_email(hf.get_email().format(i, infile, email))
+    hf.log_msg(hf.get_time(mode='end'))
 
 def main():
     card = hf.get_card()
